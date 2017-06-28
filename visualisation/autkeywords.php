4f386a71d9dc8e8d72d39f685bf7e9573be2b506
ninofiliu <nino.filiu@gmail.com>
2017-06-29 01:40:08 +0200

    Update autkeywords.php

--- visualisation/autkeywords.php
+++ visualisation/autkeywords.php
@@ -1,16 +1,17 @@
-<!-- Link the authors by their common keywords. Creates a FDG-ready-to-display JSON file -->
-
 <?php
 
-include("utils.php");
-$gad=json_decode(file_get_contents("data/groupsanddepts.json"));
+$t=time();
 
+include("utils.php");
+$gad=json_decode(file_get_contents("data/depts.json"));
 $data=json_decode(file_get_contents("data/autkeywords.json"))->results->bindings;
 for ($i=0; $i<count($data); $i++){
     $data[$i]->familyName->value=ucwords(strtolower($data[$i]->familyName->value));
 }
 
-//var_dump($data);echo "<hr>";
+$t=time()-$t;
+echo "init took $t seconds<br>";ob_flush();flush();
+$t=time();
 
 $chercheurs=[];
 foreach ($data as $d){
@@ -19,8 +20,10 @@ foreach ($data as $d){
         $chercheurs[]=$name;
     }
 }
-//var_dump($chercheurs);echo "<hr>";
 
+$t=time()-$t;
+echo "built chercheurs in $t seconds<br>";ob_flush();flush();
+$t=time();
 
 $keywords=[];
 foreach ($chercheurs as $c){
@@ -32,7 +35,10 @@ foreach ($data as $d){
 foreach ($chercheurs as $c){
     sort($keywords[$c]);
 }
-//var_dump($keywords);echo "<hr>";
+
+$t=time()-$t;
+echo "built keywords in $t seconds<br>";ob_flush();flush();
+$t=time();
 
 $co=[];
 foreach ($chercheurs as $c1){
@@ -46,14 +52,17 @@ foreach ($chercheurs as $c1){
         }
     }
 }
-//var_dump($co);echo"<hr>";
+
+$t=time()-$t;
+echo "built connexions in $t seconds<br>";ob_flush();flush();
+$t=time();
 
 $j=new stdClass();
 $j->nodes=[];
 foreach ($chercheurs as $c){
     $j->nodes[]=new stdClass();
     $j->nodes[count($j->nodes)-1]->id=$c;
-    $j->nodes[count($j->nodes)-1]->group=getGroup($c,$gad);
+    $j->nodes[count($j->nodes)-1]->group=getDept(strtolower($c),$gad);
 }
 $j->links=[];
 foreach ($co as $c1=>$c){
@@ -67,8 +76,14 @@ foreach ($co as $c1=>$c){
     }
 }
 
+$t=time()-$t;
+echo "built object in $t seconds<br>";ob_flush();flush();
+$t=time();
+
 $f=fopen("data/authorkeywords.json","w");
 fwrite($f,json_encode($j));
 fclose($f);
+
+echo "<h1>done</h1>";
 
 ?>
